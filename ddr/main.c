10d06e091670b035a0d595b2ed1e6dcae94e8ae3
Łukasz <lukasz@czak.pl>
2020-07-11 16:41:05 +0200

    [wip] Start looking into DDR initialization

---
+++ ddr/main.c
@@ -0,0 +1,28 @@
+void init();
+void blink_loop();
+
+int main() {
+	init();
+	blink_loop();
+}
+
+void blink_loop() {
+	volatile int *GPIO1_SWPORTA_DR = (int *)0xff780000;
+	volatile int *GPIO1_SWPORTA_DDR = (int *)0xff780004;
+
+	*GPIO1_SWPORTA_DDR = 0b00000001000000000000000000000000;
+
+	while (1) {
+		// LED on
+		*GPIO1_SWPORTA_DR = 0b00000001000000000000000000000000;
+
+		for (int i = 0; i < 1000000; i++)
+			;
+
+		// LED off
+		*GPIO1_SWPORTA_DR = 0;
+
+		for (int i = 0; i < 1000000; i++)
+			;
+	}
+}
